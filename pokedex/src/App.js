33db8a8c87d3736ec7405ecaf0624698ff384400
System Administrator <root@Tejeshs-MacBook-Pro-2.local>
2020-04-04 16:38:02 +0530

    setting up the template

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavBar from "./components/layout/NavBar";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
