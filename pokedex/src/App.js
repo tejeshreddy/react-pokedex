d12bf796cc3f50843ced6b7fc40dd6435f9ff5d8
System Administrator <root@Tejeshs-MacBook-Pro-2.local>
2020-04-04 17:29:24 +0530

    created pokecards

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from "./components/layout/NavBar";
+import Dasborad from './components/layout/Dasborad';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container">
+        <Dasborad />
+      </div>
     </div>
   );
 }
